037d52ddf1faa29b87defb17a48583cce03c47de
Shane <6071159+smashedr@users.noreply.github.com>
2026-01-18 20:07:37 -0800

    Fix 404 and add TODO

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // TODO: PATH: Remove base
   base: '/cssnr-vue/',
   plugins: [vue(), vueDevTools()],
   resolve: {
